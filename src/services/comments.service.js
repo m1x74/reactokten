6e39635907f52f1397567ab219800fca9f6f15d7
stasi <stasik747474@gmail.com>
2022-04-18 16:54:49 +0300

    hw4

--- src/services/comments.service.js
+++ src/services/comments.service.js
@@ -4,5 +4,5 @@ import {urls} from "../constants";
 import {axiosService} from "./axios.service";
 
 export const commentsService ={
-    getByPostId: (id)=>axiosService().get(`${urls.posts}/${id}/comments`)
+    getByPostId: (id)=>axiosService.get(`${urls.posts}/${id}/comments`)
 }
